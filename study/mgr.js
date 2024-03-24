189fe16b57a3e58f426da902eb02d664762b973b
andrewt257 <andrew.tong752@myci.csuci.edu>
2024-03-23 18:08:23 -0700

    Update mgr.js

--- study/mgr.js
+++ study/mgr.js
@@ -1,14 +1,23 @@
-define(['managerAPI'], function(Manager) {
-    var API = new Manager();
+define(['managerAPI',
+		'https://cdn.jsdelivr.net/gh/minnojs/minno-datapipe@1.*/datapipe.min.js'], function(Manager){
+
+
+	//You can use the commented-out code to get parameters from the URL.
+	//const queryString = window.location.search;
+    //const urlParams = new URLSearchParams(queryString);
+    //const pt = urlParams.get('pt');
+
+	var API    = new Manager();
+	//const subid = Date.now().toString(16)+Math.floor(Math.random()*10000).toString(16);
+	init_data_pipe(API, 'RRfyP8fV08E0',  {file_type:'csv'});	
 
     API.setName('mgr');
     API.addSettings('skip',true);
-    API.addSettings('skin','demo');
-    API.addSettings('logger',{type:'csv', url:'csv.php'});
 
-    var raceSet = API.shuffle(['a','b'])[0];
-    var blackLabels = [];
-    var whiteLabels = [];
+    //Randomly select which of two sets of category labels to use.
+    let raceSet = API.shuffle(['a','b'])[0];
+    let blackLabels = [];
+    let whiteLabels = [];
 
     if (raceSet == 'a') {
         blackLabels.push('African Americans');
@@ -18,12 +27,6 @@ define(['managerAPI'], function(Manager) {
         whiteLabels.push('White people');
     }
 
-    API.save({
-        raceSet:raceSet,
-        blackLabels:blackLabels,
-        whiteLabels:whiteLabels
-    });
-
     API.addGlobal({
         raceiat:{},
         //YBYB: change when copying back to the correct folder
@@ -31,6 +34,8 @@ define(['managerAPI'], function(Manager) {
         raceSet:raceSet,
         blackLabels:blackLabels,
         whiteLabels:whiteLabels,
+        //Select randomly what attribute words to see. 
+        //Based on Axt, Feng, & Bar-Anan (2021).
         posWords : API.shuffle([
             'Love', 'Cheer', 'Friend', 'Pleasure',
             'Adore', 'Cheerful', 'Friendship', 'Joyful', 
@@ -56,15 +61,14 @@ define(['managerAPI'], function(Manager) {
     API.addTasksSet({
         instructions: [{
             type: 'message',
-            piTemplate: true,
             buttonText: 'Continue'
         }],
 
-        realstart: [{
+        intro: [{
             inherit: 'instructions',
-            name: 'realstart',
-            templateUrl: 'realstart.jst',
-            title: 'Consent',
+            name: 'intro',
+            templateUrl: 'intro.jst',
+            title: 'Intro',
             header: 'Welcome'
         }],
 
@@ -88,47 +92,73 @@ define(['managerAPI'], function(Manager) {
             scriptUrl: 'raceiat.js'
         }],
 
-        demographics: [{
-            type: 'quest',
-            name: 'demographics',
-            scriptUrl:'demographics.js'
-        }],
-
-
-        mrscale: [{
-            type: 'quest',
-            name: 'mrscale',
-            scriptUrl:'mrscale.js'
-        }],
-
-        rwascale: [{
-            type: 'quest',
-            name: 'rwascale',
-            scriptUrl:'rwascale.js'
-        }],
-
-        debriefing: [{
-            type: 'quest',
-            name: 'debriefing',
-            scriptUrl: 'debriefing.js'
-        }],
-
         lastpage: [{
-            inherit: 'instructions',
+            type: 'message',
+            name: 'lastpage',
             templateUrl: 'lastpage.jst',
             title: 'End',
-            buttonHide: true,
+            //Uncomment the following if you want to end the study here.
+            //last:true, 
             header: 'You have completed the study'
-        }]
+        }], 
+        
+        //Use if you want to redirect the participants elsewhere at the end of the study
+        redirect:
+        [{ 
+			//Replace with any URL you need to put at the end of your study, or just remove this task from the sequence below
+            type:'redirect', name:'redirecting', url: 'https://www.google.com/search' 
+        }],
+		
+		//This task waits until the data are sent to the server.
+        uploading: uploading_task({header: 'just a moment', body:'Please wait, sending data... '})
     });
 
     API.addSequence([
-        {inherit: 'realstart'},
+        { type: 'isTouch' }, //Use Minno's internal touch detection mechanism. 
+        
+        { type: 'post', path: ['$isTouch', 'raceSet', 'blackLabels', 'whiteLabels'] },
+
+        // apply touch only styles
+        {
+            mixer:'branch',
+            conditions: {compare:'global.$isTouch', to: true},
+            data: [
+                {
+                    type: 'injectStyle',
+                    css: [
+                        '* {color:red}',
+                        '[piq-page] {background-color: #fff; border: 1px solid transparent; border-radius: 4px; box-shadow: 0 1px 1px rgba(0, 0, 0, 0.05); margin-bottom: 20px; border-color: #bce8f1;}',
+                        '[piq-page] > ol {margin: 15px;}',
+                        '[piq-page] > .btn-group {margin: 0px 15px 15px 15px;}',
+                        '.container {padding:5px;}',
+                        '[pi-quest]::before, [pi-quest]::after {content: " ";display: table;}',
+                        '[pi-quest]::after {clear: both;}',
+                        '[pi-quest] h3 { border-bottom: 1px solid transparent; border-top-left-radius: 3px; border-top-right-radius: 3px; padding: 10px 15px; color: inherit; font-size: 2em; margin-bottom: 20px; margin-top: 0;background-color: #d9edf7;border-color: #bce8f1;color: #31708f;}',
+                        '[pi-quest] .form-group > label {font-size:1.2em; font-weight:normal;}',
+
+                        '[pi-quest] .btn-toolbar {margin:15px;float:none !important; text-align:center;position:relative;}',
+                        '[pi-quest] [ng-click="decline($event)"] {position:absolute;right:0;bottom:0}',
+                        '[pi-quest] [ng-click="submit()"] {width:30%;line-height: 1.3333333;border-radius: 6px;}',
+                        // larger screens
+                        '@media (min-width: 480px) {',
+                        ' [pi-quest] [ng-click="submit()"] {width:30%;padding: 10px 16px;font-size: 1.6em;}',
+                        '}',
+                        // phones and smaller screens
+                        '@media (max-width: 480px) {',
+                        ' [pi-quest] [ng-click="submit()"] {padding: 8px 13px;font-size: 1.2em;}',
+                        ' [pi-quest] [ng-click="decline($event)"] {font-size: 0.9em;padding:3px 6px;}',
+                        '}'
+                    ]
+                }
+            ]
+        },
+        
+        
+        {inherit: 'intro'},
         {
             mixer:'random',
             data:[
                 {inherit: 'explicits'},
-                {inherit: 'demographics'},
 
                 // force the instructions to preceed the iat
                 {
@@ -141,18 +171,9 @@ define(['managerAPI'], function(Manager) {
             ]
         },
 
-        {
-            mixer:'choose',
-            n:1,
-            data:[
-                { inherit: 'mrscale' },
-                { inherit: 'rwascale' }
-            ]
-        },
-
-        {inherit: 'debriefing'},
-        { type:'postCsv'},
-        {inherit: 'lastpage'}
+		{inherit: 'uploading'},
+        {inherit: 'lastpage'},
+        {inherit: 'redirect'}
     ]);
 
     return API.script;
